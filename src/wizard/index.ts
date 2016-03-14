30d6c01d5f81e6812626b441c26a1df62fe7196a
Sven Walter <sven@k15t.com>
2016-03-14 17:59:12 +0100

    Added index.ts for wizards to make usage easier

---
+++ src/wizard/index.ts
@@ -0,0 +1,11 @@
+import {AuiNgWizardComponent} from './wizard.component';
+import {AuiNgWizardStepComponent} from './wizard-step.component';
+
+export {
+    AuiNgWizardComponent,
+    AuiNgWizardStepComponent
+};
+
+export const AUI_NG2_WIZARD_DIRECTIVES = [
+    AuiNgWizardComponent, AuiNgWizardStepComponent
+];
