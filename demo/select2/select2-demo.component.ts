e2c45d6374089c3f1a98903af024ab0f98d35a64
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2016-03-22 09:02:54 +0100

    Implement two way databinding for select2

--- demo/select2/select2-demo.component.ts
+++ demo/select2/select2-demo.component.ts
@@ -1,4 +1,4 @@
-import {Component, AfterViewChecked, OnInit} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {FORM_DIRECTIVES, CORE_DIRECTIVES} from 'angular2/common';
 import {LogService} from '../../src/services/log.service';
 import {AuiNgSelect2Component, AUI_NG2_SELECT2_DIRECTIVES} from '../../src/select2/index';
@@ -10,16 +10,27 @@ import {AuiNgCodeBlockComponent} from '../common/code-block.component';
     directives: [...FORM_DIRECTIVES, AuiNgCodeBlockComponent, ...AUI_NG2_SELECT2_DIRECTIVES],
     template: require('./select2-demo.component.html')
 })
-export class Select2DemoComponent implements AfterViewChecked {
+export class Select2DemoComponent {
 
-    items = ['JavaScript', 'CoffeeScript', 'Typescript', 'Babel'];
+    selection = [{value: 'js', label: 'Javascript'}];
+
+    items = [
+        {value: 'js', label: 'Javascript'},
+        {value: 'cs', label: 'CoffeeScript'},
+        {value: 'elm', label: 'Elm'},
+        {value: 'ng', label: 'Angular', version: 2.0},
+    ];
 
     constructor(
         private logService: LogService
     ) {}
 
-    ngAfterViewChecked() {
-        // Component views have been checked
+    addItem (option: String) {
+        this.items.push({value: 'item_' + Math.random(), label: 'Another'});
+    }
+
+    removeLast () {
+        this.selection = this.selection.slice(0, -1);
     }
 
 }
--- src/select2/select2.component.ts
+++ src/select2/select2.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from 'angular2/core';
+import {Component, Input, OnChanges, AfterViewInit, ElementRef, SimpleChange} from 'angular2/core';
 import {FORM_DIRECTIVES} from 'angular2/common';
 
 /**
@@ -8,26 +8,91 @@ import {FORM_DIRECTIVES} from 'angular2/common';
 @Component({
     selector: 'auiNgSelect2',
     directives: [...FORM_DIRECTIVES],
-    template: require('./select2.component.html')
+    template: `
+      <select class="select2" multiple>
+        <option *ngFor="#item of items" value="{{getId(item)}}">{{getLabel(item)}}</option>
+      </select>
+    `
 })
-export class AuiNgSelect2Component implements OnInit {
+export class AuiNgSelect2Component implements OnChanges, AfterViewInit {
 
-    @Input() items: Array<string>;
+    @Input() items:any[];
+    @Input() idProperty:string;
+    @Input() labelProperty:string;
+    @Input() selection:any[];
 
-    constructor() {
+    private _$select2:JQuery;
+
+    constructor(private elementRef:ElementRef) {
     }
 
-    ngOnInit() {
-        AJS.$('.select2').auiSelect2();
+    ngAfterViewInit() {
+        this.initSelect2();
+        this.updateValue();
     }
 
-    /**
-     * Sets the state of the tab. To show the tab on the screen set it active set it to true. To ensure that every time only
-     * one tab is active, please consider to activate/deactivate a tab using AuiNgTabsComponent and setActiveTab() otherwise
-     * you have to manage that manually.
-     *
-     * @param active true to set the tab active or false for inactive
-     * @returns {boolean} true if the state was changed other false e.g. in case of validation error.
-     */
+    ngOnChanges(changes:{[propertyName:string]:SimpleChange}) {
+        if (this._$select2 && changes['items'] || changes['idField'] || changes['labelField']) {
+            this.initSelect2();
+        }
 
+        if (changes['selection']) {
+            this.updateValue();
+        }
+    }
+
+    initSelect2() {
+        if (this._$select2) {
+            this._$select2.off();
+        }
+
+        this._$select2 = AJS.$(this.elementRef.nativeElement)
+            .find('.select2')
+            .auiSelect2();
+
+        this._$select2.on('change', this.updateSelection.bind(this));
+    }
+
+    updateSelection (e) {
+        if (e.removed) {
+            this.unselectItem(e.removed.id);
+        }
+
+        if (e.added) {
+            this.selectItem(e.added.id);
+        }
+    }
+
+    unselectItem (id) {
+        // using procedural solution, because binding breaks if selection is replaced with a new array
+        for (let i = 0; i < this.selection.length; i++) {
+            if (this.getId(this.selection[i]) == id) {
+                this.selection.splice(i, 1);
+            }
+        }
+    }
+
+    selectItem (id) {
+        let selected = this.items.filter((item) => this.getId(item) == id);
+        this.selection.push(selected[0]);
+    }
+
+    updateValue():void {
+        if (this._$select2) {
+            this._$select2.auiSelect2('data', this.getSelection(this.selection));
+        }
+    }
+
+    getSelection(list):[{id:string, text:string}] {
+        return list.map((item) => ({id: this.getId(item), text: this.getLabel(item)}));
+    }
+
+
+    getLabel(item:any):string {
+        return item[this.labelProperty];
+    }
+
+    getId(item:any):string {
+        return item[this.idProperty];
+    }
 }
