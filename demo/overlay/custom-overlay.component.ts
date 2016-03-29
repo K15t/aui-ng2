6bfa49cd3573ec25b21f0edba1dc8667bac89d0c
Simon Kusterer <simon@soped.com>
2016-03-29 21:29:20 +0200

    register dialog as overlay

--- demo/overlay/custom-overlay.component.ts
+++ demo/overlay/custom-overlay.component.ts
@@ -1,15 +1,14 @@
 import {Component, Inject} from 'angular2/core';
 import {OverlayRef} from '../../src/common/services/overlay.service';
+import {AuiNgDialogComponent} from '../../src/dialog/dialog.component';
 
 @Component({
     selector: 'customOverlay',
+    directives: [AuiNgDialogComponent],
     template: require('./custom-overlay.component.html')
 })
 export default class CustomOverlay {
 
     constructor(public overlay: OverlayRef) {}
 
-    close() {
-        this.overlay.remove();
-    }
 }
