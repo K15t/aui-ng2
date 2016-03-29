63a31aa86494305a8f13b2139585b2d84d961a6a
Simon Kusterer <simon@soped.com>
2016-03-29 21:46:13 +0200

    show stackable dialogs in demo

--- demo/overlay/custom-overlay.component.ts
+++ demo/overlay/custom-overlay.component.ts
@@ -1,14 +1,21 @@
-import {Component, Inject} from 'angular2/core';
+import {Component, Inject, ElementRef} from 'angular2/core';
 import {OverlayRef} from '../../src/common/services/overlay.service';
 import {AuiNgDialogComponent} from '../../src/dialog/dialog.component';
+import AuiNgOverlayService from '../../src/common/services/overlay.service';
+import AuiNgPortalService from '../../src/common/services/portal.service';
 
 @Component({
     selector: 'customOverlay',
     directives: [AuiNgDialogComponent],
+    providers: [AuiNgOverlayService, AuiNgPortalService],
     template: require('./custom-overlay.component.html')
 })
 export default class CustomOverlay {
 
-    constructor(public overlay: OverlayRef) {}
+    constructor(public overlay: OverlayRef, private _el: ElementRef, private _overlays: AuiNgOverlayService) {}
+
+    createSubDialog() {
+        this._overlays.register(CustomOverlay, this._el, {name: 'Child Dialog!'});
+    }
 
 }
--- src/common/services/overlay.service.ts
+++ src/common/services/overlay.service.ts
@@ -5,7 +5,7 @@ import {Type} from 'angular2/src/facade/lang';
 export class OverlayRef {
 
     public ref: ComponentRef;
-    public host: any;
+    public host: HTMLElement;
     public options: any;
 
     show() {
@@ -31,20 +31,14 @@ export class OverlayRef {
 @Injectable()
 export default class AuiNgOverlayService {
 
-    private _container: Element;
-
     constructor(private _portal: AuiNgPortal) {}
 
     /**
      * Register a Component as Overlay.
      */
     register(type: Type, origin: ElementRef, options?: any):Promise<OverlayRef> {
-        if (!this._container) {
-            this._createContainer();
-        }
-
         const host = document.createElement('div');
-        this._container.appendChild(host);
+        this._getContainer().appendChild(host);
 
         const overlay = new OverlayRef();
         const providers = Injector.resolve([new Provider(OverlayRef, {useValue: overlay})]);
@@ -59,10 +53,19 @@ export default class AuiNgOverlayService {
             });
     }
 
-    _createContainer(): void {
-        this._container = document.createElement('div');
-        this._container.id = 'aui_overlay_container';
-        document.body.appendChild(this._container);
+    _getContainer(): HTMLElement {
+        const containerId = 'aui_overlay_container';
+        let container = document.getElementById(containerId);
+
+        if (container) {
+            return container;
+        }
+
+        container = document.createElement('div');
+        container.id = containerId;
+        document.body.appendChild(container);
+
+        return container;
     }
 
 }
