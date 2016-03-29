ed4de13e408f41ed96a5e8fb3d1fdb3c690eb231
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2016-03-29 11:35:28 +0200

    Added tests for multi selection strategy

--- spec-bundle.js
+++ spec-bundle.js
@@ -10,14 +10,8 @@ testing.setBaseTestProviders(
   browser.TEST_BROWSER_PLATFORM_PROVIDERS,
   browser.TEST_BROWSER_APPLICATION_PROVIDERS);
 
-console.log('CALL SPEC BUNDLE');
-
 // get all the files, for each file, call the context function
 // that will require the file and load it up here. Context will
 // loop and require those spec files here
 var testContext = require.context('./src', true, /\.spec\.ts/);
-
-console.log(testContext);
-console.log(testContext.length);
-
 testContext.keys().forEach(testContext);
