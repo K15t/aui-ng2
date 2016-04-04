15daced368e69820ffdaf03fe6d017a3216ea0ad
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2016-04-04 17:13:32 +0200

    Rewrite codeBlock component

--- demo/common/code-block.component.ts
+++ demo/common/code-block.component.ts
@@ -1,4 +1,8 @@
 import {Component, Input, ViewChild, AfterViewInit} from 'angular2/core';
+const CodeMirror = require('codemirror');
+require('codemirror/lib/codemirror.css');
+require('codemirror/mode/htmlmixed/htmlmixed');
+require('codemirror/mode/javascript/javascript');
 
 @Component({
     selector: 'auiNgCodeBlock',
@@ -6,28 +10,35 @@ import {Component, Input, ViewChild, AfterViewInit} from 'angular2/core';
         <div style="padding-top: 15px">
             <h3>{{ title }}</h3>
             <div class="aui-ng2-code-block">
-                <blockquote><pre #codeBlockContent ng-non-bindable><ng-content></ng-content></pre></blockquote>
+              <div #codeBlockContent ng-non-bindable><ng-content></ng-content></div>
             </div>
         </div>
     `
 })
 export class AuiNgCodeBlockComponent implements AfterViewInit {
-    @Input() title: string;
+    @Input() title:string;
+    @Input() lang:string;
     @ViewChild('codeBlockContent') codeBlockContent;
 
-    private  htmlEscape(str) {
-        return str.replace(/&/g, '&amp;')
-            .replace(/>/g, '&gt;')
-            .replace(/</g, '&lt;')
-            .replace(/"/g, '&quot;')
-            .replace(/_sb_/g, '(')
-            .replace(/_eb_/g, ')')
-            .replace(/'/g, '&#39;')
-            .replace(/`/g, '&#96;')
-            .trim();
+    ngAfterViewInit() {
+        let textarea = this.codeBlockContent.nativeElement.querySelector('textarea');
+
+        if (textarea) {
+            CodeMirror.fromTextArea(textarea, {
+                mode: this.getMode(),
+                readOnly: true
+            });
+        }
     }
 
-    ngAfterViewInit() {
-        this.codeBlockContent.nativeElement.innerHTML = this.htmlEscape(this.codeBlockContent.nativeElement.innerHTML);
+    getMode() {
+        switch (this.lang) {
+            case 'html':
+                return 'htmlmixed';
+            case 'javascript':
+                return 'javascript';
+            default:
+                return 'htmlmixed';
+        }
     }
 }
