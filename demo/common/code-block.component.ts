9f5244769df11aa8e560af4a983cb56701ad48eb
Sven Walter <sven@k15t.com>
2016-04-13 12:20:36 +0200

    Fix js error clicking into a code block example, change style of the code blocks, updated angular to the latest beta, avoid empty line at the end of the code block

--- demo/common/code-block.component.ts
+++ demo/common/code-block.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, ViewChild, AfterViewInit} from 'angular2/core';
+
 const CodeMirror = require('codemirror');
-require('codemirror/lib/codemirror.css');
 require('codemirror/mode/htmlmixed/htmlmixed');
 require('codemirror/mode/javascript/javascript');
 
@@ -16,34 +16,41 @@ require('codemirror/mode/javascript/javascript');
     `
 })
 export class AuiNgCodeBlockComponent implements AfterViewInit {
-    @Input() title:string;
-    @Input() lang:string;
+    @Input() title: string;
+    @Input() lang: string;
     @ViewChild('codeBlockContent') codeBlockContent;
 
     ngAfterViewInit() {
         let textarea = this.codeBlockContent.nativeElement.querySelector('textarea');
-
         if (textarea) {
+            // ... remove the last line break to avoid an empty row
+            if (textarea.value && textarea.value !== undefined) {
+                textarea.value = textarea.value.replace(/\s+$/g, '');
+            }
             let editor = CodeMirror.fromTextArea(textarea, {
                 mode: this.getMode(),
-                readOnly: true
+                readOnly: 'nocursor',
+                lineNumbers: true,
+                lineWrapping: false,
+                gutter: true,
+                autofocus: false,
+                dragDrop: false,
+                showCursorWhenSelecting: false
             });
-
-            AuiNgCodeBlockComponent.fixIndentation(editor);
+            this.fixIndentation(editor);
         }
     }
 
-    static fixIndentation(editor) {
+    private fixIndentation(editor) {
         let line = editor.getLine(0);
         let indentation = line.match(/^\W*/)[0].length;
-
 
         for (let i = 0; i < editor.lineCount(); i++) {
             editor.indentLine(i, -indentation);
         }
     }
 
-    getMode() {
+    private getMode() {
         switch (this.lang) {
             case 'html':
                 return 'htmlmixed';
