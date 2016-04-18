3f338e5f6a44a59fd675488143b24738a37d7468
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2016-04-18 13:01:14 +0200

    Add orientation option

--- src/radio-button-group/radio-button-group.component.ts
+++ src/radio-button-group/radio-button-group.component.ts
@@ -4,21 +4,28 @@ import {FORM_DIRECTIVES} from 'angular2/common';
 @Component({
     selector: 'auiNgRadioButtonGroup',
     directives: [...FORM_DIRECTIVES],
+    styles: [`
+      .aui-ng2-orientation-horizontal .radio{
+        float: left;
+        margin-right: 10px;
+      }
+    `],
     template: `
-      <fieldset class="group">
+      <fieldset class="group aui-ng2-orientation-{{orientation}}">
         <legend><span>{{title}}</span></legend>
         <div class="radio"
              *ngFor="#item of items">
-            <input class="radio" type="radio" name="radioButtonGroup{{title}}"
-                   id="radioButtonGroup{{title}}{{getId(item)}}"
+            <input class="radio" type="radio" name="{{name}}"
+                   id="{{name}}{{getId(item)}}"
                    (change)="onChange(item)"
                    [checked]="getId(selection) == getId(item)">
-            <label attr.for="radioButtonGroup{{title}}{{getId(item)}}">{{getLabel(item)}}</label>
+            <label attr.for="{{name}}-{{getId(item)}}">{{getLabel(item)}}</label>
         </div>
       </fieldset>
     `
 })
 export class AuiNgRadioButtonGroupComponent {
+    @Input() orientation: string;
     @Input() title: string;
     @Input() selection: any;
     @Input() items: any[];
