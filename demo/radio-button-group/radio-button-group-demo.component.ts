a2de9ac870679c9c8e05919b1d9e173c69836a8e
Sven Walter <sven@k15t.com>
2016-04-22 15:38:54 +0200

    Added interface to improve typing

--- demo/radio-button-group/radio-button-group-demo.component.ts
+++ demo/radio-button-group/radio-button-group-demo.component.ts
@@ -3,19 +3,27 @@ import {FORM_DIRECTIVES} from 'angular2/common';
 import {AUI_NG2_RADIO_BUTTON_GROUP_DIRECTIVES} from '../../src/radio-button-group/index';
 import {AuiNgCodeBlockComponent} from '../common/code-block.component';
 
+/**
+ * Simple color item consisting of id and name.
+ */
+interface ColorItem {
+    name: string;
+    id: number;
+}
+
 @Component({
     selector: 'demoRadioButtonGroup',
     directives: [...FORM_DIRECTIVES, AuiNgCodeBlockComponent, ...AUI_NG2_RADIO_BUTTON_GROUP_DIRECTIVES],
     template: require('./radio-button-group-demo.component.html')
 })
 export class RadioButtonGroupDemoComponent {
-    private colorItems: any = [
+    private colorItems: Array<ColorItem> = [
         {name: 'yellow', id: 1},
         {name: 'blue', id: 2},
         {name: 'green', id: 3},
         {name: 'red', id: 4}
     ];
 
-    private color1: any = this.colorItems[1];
-    private color2: any;
+    private color1 = this.colorItems[1];
+    private color2;
 }
