be9000cc1817539a9b4aa8b9fbe2d79b2403daa5
Sven Walter <sven@k15t.com>
2016-04-24 11:14:42 +0200

    Setup  auto. deployment as part of the release for the demo

--- webpack-config.js
+++ webpack-config.js
@@ -2,15 +2,16 @@ var master = require('k15t-webpack-build/webpack-master-config.js');
 var utils = require('k15t-webpack-build/utils.js');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 var path = require('path');
 
-if (process.env.testMode === 'false') {
+if (process.env.testMode === 'true') {
     module.exports = master({
         basePath: '',
         autoWatch: true,
         autoWatchBatchDelay: 300,
         output: {
-            path : './target'
+            path: './target'
         },
         module: {
             noParse: [
@@ -23,25 +24,43 @@ if (process.env.testMode === 'false') {
     module.exports = master({
         entry: {
             main: './demo/main.ts',
-            vendor: ['./demo/vendor.ts']
+            vendor: './demo/vendor.ts'
+        },
+        envProperties: {
+            PRODUCT_VERSION: require("./package.json").version,
+            BUILD_TIME: (new Date).toLocaleString(),
+            SERVER_CONTEXT: process.env.releaseDemo === 'true' ? '/aui-ng2/' : ''
         },
         output: {
             filename: './demo/[name].min.js',
             sourceMapFilename: './demo/[name].map',
             chunkFilename: './demo/[id].chunk.js',
-            path: !!process.env.release ? './dist' : './target'
+            path: process.env.release === 'true' ? './dist' : './target'
         },
-        plugins: !!process.env.release ? [
-            new CopyWebpackPlugin([{
-                from: 'src',
-                to: '.'
-            }])
-        ] : [
-            new HtmlWebpackPlugin({
+        addPlugins: function(devModeEnabled, testingEnabled, debugModeEnabled) {
+
+            var plugins = [];
+
+            if (process.env.release === 'true') {
+                plugins.push(new CopyWebpackPlugin([{
+                    from: 'src',
+                    to: '.'
+                }]));
+            }
+
+            plugins.push(new HtmlWebpackPlugin({
                 filename: 'index.html',
                 template: './demo/index.html',
                 inject: false
-            })
-        ]
+            }));
+
+            plugins.push(new CommonsChunkPlugin({
+                name: 'vendor',
+                filename: './demo/[name].min.js',
+                minChunks: Infinity
+            }));
+
+            return plugins;
+        }
     });
 }
